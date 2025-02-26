4115bb30c42606d8d1870c5a8c5d94083152ab43
wiwiwuwuwa <wiwiwuwuwa@gmail.com>
2025-02-27 00:35:51 +0500

    added commentaries

--- Projects/WinApi/Includes/WinApi.hpp
+++ Projects/WinApi/Includes/WinApi.hpp
@@ -3,8 +3,26 @@
 
 extern "C" { namespace Aiva::WinApi
 {
-    void* __stdcall GetStdHandle(MinLib::uint32_t const);
-    MinLib::int32_t __stdcall WriteFile(void*const, void const*const, MinLib::uint32_t const, MinLib::uint32_t *const, void *const);
-    void __stdcall ExitProcess(MinLib::uint32_t const);
+    // HANDLE WINAPI GetStdHandle
+    // (
+        // _In_ DWORD nStdHandle
+    // );
+    void* __stdcall GetStdHandle(MinLib::uint32_t const nStdHandle);
+
+    // BOOL WriteFile
+    // (
+        // [in]                HANDLE       hFile,
+        // [in]                LPCVOID      lpBuffer,
+        // [in]                DWORD        nNumberOfBytesToWrite,
+        // [out, optional]     LPDWORD      lpNumberOfBytesWritten,
+        // [in, out, optional] LPOVERLAPPED lpOverlapped
+    // );
+    int __stdcall WriteFile(void *const hFile, void const*const lpBuffer, MinLib::uint32_t const nNumberOfBytesToWrite, MinLib::uint32_t *const lpNumberOfBytesWritten, void *const lpOverlapped);
+
+    // void ExitProcess
+    // (
+        // [in] UINT uExitCode
+    // );
+    void __stdcall ExitProcess(MinLib::uint32_t const uExitCode);
 }}
 // extern "C" namespace Aiva::WinApi
