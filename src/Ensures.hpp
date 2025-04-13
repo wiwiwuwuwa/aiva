cf19c39171d9c674bcb6a7762409d7ec548f2a95
wiwiwuwuwa <wiwiwuwuwa@gmail.com>
2025-04-14 01:45:51 +0500

    add print line number to check no entry

--- src/Ensures.hpp
+++ src/Ensures.hpp
@@ -1,12 +1,19 @@
 #pragma once
 
 
+#define CheckNoEntry_Stringify(x) #x
+#define CheckNoEntry_ToString(x) CheckNoEntry_Stringify(x)
+#define CheckNoEntry_GetCodeLine() CheckNoEntry_ToString(__LINE__)
+
+
 #define CheckNoEntry() \
 { \
     Aiva::CstrView const messages[] = \
     { \
         __FILE__, \
-        ": ", \
+        "(", \
+        CheckNoEntry_GetCodeLine(), \
+        "): ", \
         __func__, \
         ": NO_ENTRY_POINT." \
     }; \
