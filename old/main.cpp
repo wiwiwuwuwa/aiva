6f4b16b1bc0a8773e74168e02221c40b5e3240c0
wiwiwuwuwa <wiwiwuwuwa@gmail.com>
2026-06-11 02:07:22 +0700

    start new version

---
+++ old/main.cpp
@@ -0,0 +1,19 @@
+#include "aiva.hpp"
+
+
+void main()
+{
+    Aiva::Console::InitSystem();
+    Aiva::Memory::InitSystem();
+    Aiva::Coroutines::InitSystem();
+    Aiva::Windows::InitSystem();
+
+    while (true) {}
+
+    Aiva::Windows::ShutSystem();
+    Aiva::Coroutines::ShutSystem();
+    Aiva::Memory::ShutSystem();
+    Aiva::Console::ShutSystem();
+
+    Aiva::System::ExitSuccess();
+}
--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,5 @@
 
 void main()
 {
-    Aiva::Console::InitSystem();
-    Aiva::Memory::InitSystem();
-    Aiva::Coroutines::InitSystem();
-    Aiva::Windows::InitSystem();
-
     while (true) {}
-
-    Aiva::Windows::ShutSystem();
-    Aiva::Coroutines::ShutSystem();
-    Aiva::Memory::ShutSystem();
-    Aiva::Console::ShutSystem();
-
-    Aiva::System::ExitSuccess();
 }
