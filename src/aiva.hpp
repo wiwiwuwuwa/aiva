5e5ee7d7b9c62d190533bf79a262db99066f768d
wiwiwuwuwa <wiwiwuwuwa@gmail.com>
2026-06-11 04:37:59 +0700

    add non copyable

--- src/aiva.hpp
+++ src/aiva.hpp
@@ -207,3 +207,24 @@ namespace Aiva
     using size_t = decltype(sizeof(byte_t));
     using nullptr_t = decltype(nullptr);
 }
+
+
+// ------------------------------------
+// "non_copyable.hpp"
+
+
+namespace Aiva
+{
+    class NonCopyable
+    {
+    public:
+        NonCopyable(NonCopyable const&) = delete;
+        NonCopyable& operator=(NonCopyable const&) = delete;
+        NonCopyable(NonCopyable&&) = delete;
+        NonCopyable& operator=(NonCopyable&&) = delete;
+
+    protected:
+        NonCopyable() = default;
+        ~NonCopyable() = default;
+    };
+}
